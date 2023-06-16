d4d3d4e74c4fe11deb2eaee0a36c9b647538727f
Raúl Gonzalez <rgonzalef@gmail.com>
2023-06-15 22:22:36 -0500

    feat:add expressRoutes, send File

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 
 function getMessages(req, res) {
-    res.send('<h1>Hello Albert!!</h1>')
+    //res.send('<h1>Hello Albert!!</h1>')
+    res.sendFile(path.join(__dirname, '..', 'public', 'code.png'))
   }
 
 function postMessages(req, res) {
--- express-project/server.js
+++ express-project/server.js
@@ -1,29 +1,23 @@
 const express = require('express');
 
-const messagesController = require('./controllers/messages.controller')
-const friendsController = require('./controllers/friends.controller')
+const friendsRouter = require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router')
 
 const app = express();
 
 const PORT = 3000;
 
-
-
   app.use((req, res, next) => {
     const start = Date.now();
     next();
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
   })
 
   app.use(express.json());
-  
-  app.post('/friends', friendsController.postFriend )
-  app.get('/friends', friendsController.getAllFriends )
-  app.get('/friends/:friendId', friendsController.getFriend )
 
-  app.get('/messages', messagesController.getMessages)
-  app.post('/messages', messagesController.postMessages )
+  app.use('/friends', friendsRouter)
+  app.use('/messages', messagesRouter )
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}...`)
